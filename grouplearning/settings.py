e09fa15ffd4740ab0bc23a6a18f8d4a05d04b395
kemtis <60010107@kmitl.ac.th>
2020-06-21 16:01:22 +0700

    resolved the CORS policy, done:)

--- grouplearning/settings.py
+++ grouplearning/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'authapp',
     'drf_yasg',
-    'djoser'
+    'djoser',
+    'corsheaders',
 ]
 
 JWT_AUTH = {
@@ -58,7 +59,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'grouplearning.urls'
 
